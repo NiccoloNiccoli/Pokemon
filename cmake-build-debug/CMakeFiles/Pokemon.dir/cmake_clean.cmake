3f3ce0c06df7a27e21a6afae641afc25255c91ee
NiccoloNiccoli <niccolo.niccoli@stud.unifi.it>
2020-05-20 20:01:23 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/Pokemon.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pokemon.dir/main.cpp.obj"
+  "Pokemon.exe"
+  "Pokemon.exe.manifest"
+  "Pokemon.pdb"
+  "libPokemon.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pokemon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
