1817614ce011e159efc4183fe4045b856d14ced1
NiccoloNiccoli <65605384+NiccoloNiccoli@users.noreply.github.com>
2020-05-21 18:21:24 +0200

    Add files via upload
    Game loop added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int width = 800, height = 600;
+    sf::RenderWindow window(sf::VideoMode(width, height), "Pokemon");
+    window.setFramerateLimit(60);
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event))
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+        window.clear();
+
+        //draw everything here ( window.draw(...); )
+
+        window.display();
+    }
     return 0;
 }
