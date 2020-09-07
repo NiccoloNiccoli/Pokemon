346ebb4bd3353d1f68de5aa3a72cf2ee902b1c41
NiccoloNiccoli <niccolo.niccoli@stud.unifi.it>
2020-09-07 16:42:06 +0200

    Fixed some small things

--- cmake-build-release/Google_tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/super/CLionProjects/Pokemon_cloned/Google_tests/lib
-# Build directory: C:/Users/super/CLionProjects/Pokemon_cloned/cmake-build-release/Google_tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-release/Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/super/CLionProjects/Pokemon_cloned/Google_tests/lib/googletest
-# Build directory: C:/Users/super/CLionProjects/Pokemon_cloned/cmake-build-release/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
